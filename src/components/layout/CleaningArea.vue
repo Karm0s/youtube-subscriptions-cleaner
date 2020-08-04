1df59c0c431037dfe98e62ebb8c4e6f73aab0f9c
Karm0s <bouchenna.yanis@protonmail.com>
2020-08-04 15:58:53 +0100

    - Renamed FilterChannels to Filter - Filter component layout

--- src/components/layout/CleaningArea.vue
+++ src/components/layout/CleaningArea.vue
@@ -3,20 +3,20 @@
     <v-alert type="info" border="left" class="mb-0" colored-border elevation="2">
       Please select all the channels you want to unsubscribe from and then hit the 'Done' button at the bottom right of the page
     </v-alert>
-    <FilterChannels/>
+    <Filter/>
     <Channels />
     <v-btn fixed bottom right color="red" elevation="15" width="100">Done</v-btn>
   </v-container>
 </template>
 
 <script>
-import FilterChannels from './FilterChannels';
+import Filter from './Filter';
 import Channels from './Channels';
 
 export default {
   name: "CleaningArea",
   components: {
-    FilterChannels,
+    Filter,
     Channels
   }
 }
